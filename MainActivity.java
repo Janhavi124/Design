cb102b7d264ab5d94e7984986f8982a6c5e95632
Janhavi124 <janhavi124@gmail.com>
2018-06-13 15:03:37 -0400

    Axure Grocery

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.janhavi.projectsenior;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.Image;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.List;
+import java.util.Locale;
+
+
+public class MainActivity extends AppCompatActivity  {
+
+
+    public String[] fruits = new String[2];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
